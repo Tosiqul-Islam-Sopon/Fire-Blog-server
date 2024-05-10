80edc10bcade701c3d43036935da8252a9767af2
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-05-11 01:11:57 +0600

    get blog of any specif id

--- index.js
+++ index.js
@@ -12,47 +12,62 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nnvexxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const database = client.db("FireBlogDB");
-    const blogCollection = database.collection("Blogs");
+        const database = client.db("FireBlogDB");
+        const blogCollection = database.collection("Blogs");
 
-    app.post("/addBlog", async(req, res) =>{
-        const blog = req.body;
-        const result = await blogCollection.insertOne(blog);
-        res.send(result);
-    })
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.get("/latestBlogs", async (req, res) => {
+            const result = await blogCollection.find()
+                .sort({ uploadDateTime: -1 })
+                .limit(6)
+                .toArray();
+            res.send(result);
+        });
+
+        app.get("/blog/:id", async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await blogCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post("/addBlog", async (req, res) => {
+            const blog = req.body;
+            const result = await blogCollection.insertOne(blog);
+            res.send(result);
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.send("FireBlog server is Running");
 })
 
-app.listen(port , () =>{
+app.listen(port, () => {
     console.log(`FireBlog server is running on ${port}`);
 })
