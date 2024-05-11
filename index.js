834623ac29d82aa70b1777da02111b75d7c3825a
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-05-11 23:37:59 +0600

    delete wishlist from database done

--- index.js
+++ index.js
@@ -116,6 +116,13 @@ async function run() {
             const result = await wishlistCollection.insertOne(wishlist);
             res.json(result);
         });
+
+        app.delete("/wishlist/:id", async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await wishlistCollection.deleteOne(query);
+            res.send(result);
+        })
         
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
